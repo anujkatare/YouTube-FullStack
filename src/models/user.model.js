dee4671f770fd06a592336e96fe7d8ac5cba3b95
anujkatare <anujkatare2006@gmail.com>
2026-01-20 00:08:23 +0530

    integrated JWT

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -61,5 +61,32 @@ userSchema.methods.isPassowrdCorrect = async function (password) {
     return await bcrypt.compare(password, this.password)
 }
 
+userSchema.methods.generateAccessToken = async function (){
+    return await jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefeshToken = async function (){
+    return await jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn:process.env.REFRESH_TOKEN_SECRET
+        }
+    )
+}
+
 
 export const User = mongoose.model('User', userSchema)
