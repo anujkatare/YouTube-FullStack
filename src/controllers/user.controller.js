8515882018a25b39890c35d3862c4429cbe9c717
anujkatare <anujkatare2006@gmail.com>
2026-01-22 21:31:45 +0530

    User logout declaration

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -110,7 +110,28 @@ const loginUser = asyncHandler(async (req, res ) => {
 })
 
 const logoutUser = asyncHandler( async (req, res) => {
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken: undefined
+            }
+        },{
+            new: true
+        }
+    )
     
+    const options = {
+    httpOnly: true,
+    secure: true
+   }
+
+   return res
+   .status(200)
+   .clearCookie("accessToken", options)
+   .clearCookie("refreshToken", options)
+   .json("User logged out")
+
 })
 
-export {registerUser, loginUser} 
+export {registerUser, loginUser, logoutUser} 
---
+++ src/middlewares/auth.js
@@ -0,0 +1,32 @@
+import { asyncHandler } from "../utils/asyncHandler.js"
+import JWT from 'jsonwebtoken'
+import {User} from "../models/user.model.js"
+
+
+export default verifyJWT = asyncHandler(async (req, res, next) => {
+    try {
+        const Token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+    
+        if(!token){
+            res.status(401)
+            console.error("Unauthorized request")
+        }
+    
+        const decodedToken = JWT.verify(token, process.env.ACCESS_TOKEN_SECRET)
+    
+        const user = await User
+        .findById(decodedToken?._id)
+        .select("-password -refreshToken")
+    
+        if(!user){
+            res.status(401)
+            console.log("Invalid Access Token")
+        }
+    
+        req.user = user
+        next()
+    } catch (error) {
+        res.status(401)
+        console.error("Error (Invalid Acess Token):", error)
+    }
+})
